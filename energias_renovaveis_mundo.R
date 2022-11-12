5f3d5599c146049c5d58f4aefc170c1fc2128fc1
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-11-11 23:29:26 -0300

    Add files via upload

--- energias_renovaveis_mundo.R
+++ energias_renovaveis_mundo.R
@@ -15,5 +15,41 @@ library(gridExtra)
 
 energy <- read.csv("renewable-share-energy.csv")
 view(energy)
+names(energy)
 energy_fonte <- read.csv("modern-renewable-prod.csv")
 view(energy_fonte)
+names(energy_fonte)
+
+# Manipular dados --------------------------------------------------------------------------------------------------------------------------
+
+energy <- energy %>%
+  select(-Code) %>%
+  rename(por_energ = Renewables....equivalent.primary.energy.) %>%
+  view()
+
+# Para a América Latina
+
+energy1 <- energy %>%
+  filter(Entity %in% c("Argentina", "Bolivia", "Brazil", "Chile",
+                       "Colombia", "Costa Rica", "Cuba", 
+                       "Dominican Republic", "Ecuador", "El Salvador",
+                       "Guatemala", "Haiti", "Honduras", "Mexico",
+                       "Nicaragua", "Panama", "Paraguay", "Peru",
+                       "Uruguay", "Venezuela")) %>%
+  group_by(Entity) %>%
+  summarise(media = mean(por_energ),
+            sd = sd(por_energ), n = n(),
+            se = sd/sqrt(n)) %>%
+  view()
+
+energy2 <- energy %>%
+  filter(Entity %in% c("Argentina", "Bolivia", "Brazil", "Chile",
+                       "Colombia", "Costa Rica", "Cuba", 
+                       "Dominican Republic", "Ecuador", "El Salvador",
+                       "Guatemala", "Haiti", "Honduras", "Mexico",
+                       "Nicaragua", "Panama", "Paraguay", "Peru",
+                       "Uruguay", "Venezuela")) %>%
+  view()
+
+
+

1476c97913d1ec7f9e7f877466cc000eef0798e8
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-11-13 03:17:43 -0300

    Add files via upload

--- energias_renovaveis_mundo.R
+++ energias_renovaveis_mundo.R
@@ -31,7 +31,8 @@ energy <- energy %>%
 
 energy3 <- energy %>%
     filter(Entity %in% c("China", "Japan", "Germany", 
-                         "United Kingdom", "United States")) %>%
+                         "United Kingdom", "United States"),
+           Year %in% c("2018", "2019", "2020", "2021")) %>%
   group_by(Entity) %>%
   summarise(media = mean(por_energ),
             sd = sd(por_energ), n = n(),
@@ -52,14 +53,14 @@ g3 <- ggplot(energy3, aes(x = fct_reorder(Entity, media),
   geom_col(width = 0.9) +
   geom_errorbar(aes(ymin = media - se, ymax = media + se),
                 width = 0.3, size = 0.8) +
-  scale_fill_manual(values = c("#E69F00", "#56B4E9", 
-                                "#009E73", "#F0E442", 
-                                "#0072B2")) +
+  scale_fill_manual(values = c("#1B9E77", "#D95F02", 
+                                "#7570B3", "#E7298A", 
+                                "#66A61E")) +
   scale_y_continuous(expand = expansion(mult = c(0,0.1))) +
   coord_flip() +
   labs(x = "Países", 
        y = "Porcentagem",
-       title = "Energia primária de fontes renováveis\n entre os anos de 2000 e 2021") +
+       title = "Energia primária de fontes renováveis\n entre os anos de 2018 e 2021") +
   theme_light() +
   theme(legend.position = "none",
         axis.text = element_text(color = "black"))
@@ -68,13 +69,13 @@ g3
 g4 <- ggplot(energy4, aes(x = Year, y = por_energ, 
                     group = Entity, color = Entity)) +
   geom_line(size = 1.3) +
-  scale_color_manual(values = c("#2E91E5", "#E15F99", 
-                                "#1CA71C", "#FB0D0D", 
-                                "#DA16FF")) +
+  scale_color_manual(values = c("#1B9E77", "#D95F02", 
+                                 "#7570B3", "#E7298A", 
+                                 "#66A61E")) +
   scale_x_continuous(expand = expansion(mult = c(0.01,0.01))) +
   labs(x = "Tempo (anos)", 
        y = "Porcentagem",
-       title = "Energia primária de fontes renováveis\n entre os anos de 2000 e 2021",
+       title = "Energia primária de fontes renováveis\n entre os anos de 1965 e 2021",
        color = "Países") +
   theme_light() +
   theme(axis.text = element_text(color = "black"))

4e2f39319466fea3e32e68a19089a8d0cfe570b7
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-11-13 01:41:04 -0300

    Add files via upload

--- energias_renovaveis_mundo.R
+++ energias_renovaveis_mundo.R
@@ -27,27 +27,14 @@ energy <- energy %>%
   rename(por_energ = Renewables....equivalent.primary.energy.) %>%
   view()
 
-# Para os países mais desenvolvidos da América Latina
-
-energy1 <- energy %>%
-  filter(Entity %in% c("Argentina", "Brazil", "Chile", 
-                       "Peru", "Colombia", "Mexico")) %>%
-  group_by(Entity) %>%
-  summarise(media = mean(por_energ),
-            sd = sd(por_energ), n = n(),
-            se = sd/sqrt(n)) %>%
-  view()
-
-energy2 <- energy %>%
-  filter(Entity %in% c("Argentina", "Brazil", "Chile", 
-                       "Peru", "Colombia", "Mexico")) %>%
-  view()
-
 # Para os países com as maiores economias do mundo
 
 energy3 <- energy %>%
-    filter(Entity %in% c("China", "Japan", "Germany", "United Kingdom", "India", 
-                       "France", "Italy", "United States", "Canada", "South Korea"),
+    filter(Entity %in% c("China", "Japan", "Germany", 
+                         "United Kingdom", "India", 
+                       "France", "Italy", "United States", 
+                       "Canada", "South Korea", "Russia",
+                       "Australia"),
            between(Year, 2000, 2021)) %>%
   group_by(Entity) %>%
   summarise(media = mean(por_energ),
@@ -56,60 +43,29 @@ energy3 <- energy %>%
   view()
 
 energy4 <- energy %>%
-    filter(Entity %in% c("China", "Japan", "Germany", "United Kingdom", "India", 
-                       "France", "Italy", "United States", "Canada", "South Korea"),
+    filter(Entity %in% c("China", "Japan", "Germany", 
+                         "United Kingdom", "India", 
+                       "France", "Italy", "United States", 
+                       "Canada", "South Korea", "Russia",
+                       "Australia"),
            between(Year, 2000, 2021)) %>%
   view()
 
 # Gráficos ---------------------------------------------------------------------------------------------------------------------------------
 
-g1 <- ggplot(energy3, aes(x = fct_reorder(Entity, media), 
-                    y = media, fill = Entity)) +
-  geom_col(width = 0.9) +
-  geom_errorbar(aes(ymin = media - se, ymax = media + se),
-                width = 0.3, size = 0.8) +
-  scale_fill_manual(values = c("#7F3C8D", "#11A579", 
-                                "#3969AC", "#F2B701", 
-                                "#E73F74", "#80BA5A", 
-                                "#E68310", "#008695", 
-                                "#CF1C90", "#A5AA99")) +
-  scale_y_continuous(expand = expansion(mult = c(0,0.1))) +
-  coord_flip() +
-  labs(x = "Países", 
-       y = "Porcentagem",
-       title = "Energia primária de fontes renováveis\n entre os anos de 2000 e 2021") +
-  theme_light() +
-  theme(legend.position = "none",
-        axis.text = element_text(color = "black"))
-g1
-
-g2 <- ggplot(energy4, aes(x = Year, y = por_energ, 
-                    group = Entity, color = Entity)) +
-  geom_line(size = 1.8) +
-  scale_color_manual(values = c("#7F3C8D", "#11A579", 
-                                "#3969AC", "#F2B701", 
-                                "#E73F74", "#80BA5A", 
-                                "#E68310", "#008695", 
-                                "#CF1C90", "#A5AA99")) +
-  scale_x_continuous(expand = expansion(mult = c(0,0))) +
-  labs(x = "Tempo (anos)", 
-       y = "Porcentagem",
-       title = "Energia primária de fontes renováveis\n entre os anos de 2000 e 2021",
-       color = "Países") +
-  theme_light() +
-  theme(axis.text = element_text(color = "black"))
-g2
+c4a_gui()
 
 g3 <- ggplot(energy3, aes(x = fct_reorder(Entity, media), 
                     y = media, fill = Entity)) +
   geom_col(width = 0.9) +
   geom_errorbar(aes(ymin = media - se, ymax = media + se),
                 width = 0.3, size = 0.8) +
-  scale_fill_manual(values = c("#7F3C8D", "#11A579", 
-                                "#3969AC", "#F2B701", 
-                                "#E73F74", "#80BA5A", 
-                                "#E68310", "#008695", 
-                                "#CF1C90", "#A5AA99")) +
+  scale_fill_manual(values = c("#575757", "#AD2323", 
+                                "#2A4BD7", "#1D6914", 
+                                "#814A19", "#8126C0", 
+                                "#A0A0A0", "#81C57A", 
+                                "#9DAFFF", "#29D0D0", 
+                                "#FF9233", "#FFEE33")) +
   scale_y_continuous(expand = expansion(mult = c(0,0.1))) +
   coord_flip() +
   labs(x = "Países", 
@@ -122,13 +78,14 @@ g3
 
 g4 <- ggplot(energy4, aes(x = Year, y = por_energ, 
                     group = Entity, color = Entity)) +
-  geom_line(size = 1.8) +
-  scale_color_manual(values = c("#7F3C8D", "#11A579", 
-                                "#3969AC", "#F2B701", 
-                                "#E73F74", "#80BA5A", 
-                                "#E68310", "#008695", 
-                                "#CF1C90", "#A5AA99")) +
-  scale_x_continuous(expand = expansion(mult = c(0,0))) +
+  geom_line(size = 1.3) +
+  scale_color_manual(values = c("#575757", "#AD2323", 
+                                "#2A4BD7", "#1D6914", 
+                                "#814A19", "#8126C0", 
+                                "#A0A0A0", "#81C57A", 
+                                "#9DAFFF", "#29D0D0", 
+                                "#FF9233", "#FFEE33")) +
+  scale_x_continuous(expand = expansion(mult = c(0.01,0.01))) +
   labs(x = "Tempo (anos)", 
        y = "Porcentagem",
        title = "Energia primária de fontes renováveis\n entre os anos de 2000 e 2021",
@@ -136,7 +93,7 @@ g4 <- ggplot(energy4, aes(x = Year, y = por_energ,
   theme_light() +
   theme(axis.text = element_text(color = "black"))
 g4
-
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
 
 
 
